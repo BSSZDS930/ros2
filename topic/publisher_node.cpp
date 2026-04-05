65d8f7e18c66a1037c11e726525fa9db1df0782a
GYL <1053978756@qq.com>
2026-04-05 12:34:22 +0800

    hint

--- topic/publisher_node.cpp
+++ topic/publisher_node.cpp
@@ -6,14 +6,17 @@ int main(int argc,char *argv[])
     rclcpp::init(argc, argv);
 	
 	auto node = std:make_shared<rclcpp::Node>("publisher_node");
-	
+
+	// 创建一个发布者对象，参数：(发布的话题，发送缓冲的队列大小）
 	auto publisher = node->create_publisher<std_msgs::msg::String>("/my_topic", 10);
 	
 	std_msgs::msg::String message;
 	message.data = "hello world";
-	
+
+	// 循环频率对象，1Hz，即每秒循环一次
 	rclcpp::Rate loop_rate(1);
-	
+
+	// 检查ROS2是否正在运行
 	while(rclcpp::ok()) {
 		publisher->publish(message);
 		loop_rate.sleep();
--- topic/subscriber_node.cpp
+++ topic/subscriber_node.cpp
@@ -12,11 +12,15 @@ int main(int argc, char* argv)
 {
     rclcpp::init(argc, argv);
 
+    // 创建一个节点
     node = std::make_shared<rclcpp::Node>("subscriber_node");
 
-    auto  subscriber = node->create_subscription<std_msgs::msg::String>("/my_topic", 10 &Callback);
+    // 创建一个订阅者对象，参数：(订阅的话题，接收消息包的缓冲队列大小，回调函数）
+    auto  subscriber = node->create_subscription<std_msgs::msg::String>("/my_topic", 10, &Callback);
 
+    // 启动节点的主循环，开启消息接收处理，直至收到系统的关闭要求。
     rclcpp::spin(node);
+
     rclcpp::shutdown();
 
     return 0;
